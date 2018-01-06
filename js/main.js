ba3e6e4b2683ded3d8ac8841af4dc2715d45c431
Haley Ostrander <h_ostrander@outlook.com>
2018-01-06 18:48:46 -0500

    fluid mobile and desktop styles

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 // JavaScript Document
+
+var menuToggler = document.querySelector('#main-nav');
+var menu = document.querySelector('.navigation-menu');
+var hamAnimation = document.querySelector('#menuToggle');
+var navText = document.querySelectorAll('.navText');
+
+function toggleMenu() {
+	// console.log(menuToggler);
+	menu.classList.toggle('active-nav');
+	hamAnimation.classList.toggle('open-menu');
+}
+
+navText.forEach(function(e, i) {
+	navText[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		smoothScroll(navText[i]);
+		toggleMenu();
+	}, false);
+});
+
+menuToggler.addEventListener('click', toggleMenu, false);
+
+function smoothScroll(e) {
+	console.log(e);
+	var elementToScroll = e.getAttribute("href");
+	console.log(elementToScroll);
+	document.querySelector(elementToScroll).scrollIntoView({ 
+	  behavior: 'smooth' 
+	});
+
+}
